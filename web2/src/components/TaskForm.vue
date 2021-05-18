2afb0ce33e4b2cf5cf19946d7c8d64a8cf513937
bukharovev <bukharovev@gmail.com>
2021-05-18 09:38:57 +0300

    added option: select host

--- web2/src/components/TaskForm.vue
+++ web2/src/components/TaskForm.vue
@@ -30,12 +30,29 @@
       :disabled="formSaving"
       rows="4"
     ></v-textarea>
+
+    <v-select
+      label="Select host"
+      v-model="item.host"
+      :items="hosts"
+      item-value="id"
+      item-text="host-name"
+      :rules="[v => !!v || 'Host is required']"
+      required
+      :disabled="formSaving"
+    ></v-select>
   </v-form>
 </template>
 <script>
+import axios from 'axios';
 import ItemFormBase from '@/components/ItemFormBase';
 
 export default {
+  data() {
+    return {
+      hosts: [],
+    };
+  },
   mixins: [ItemFormBase],
   props: {
     templateId: Number,
@@ -51,13 +68,31 @@ export default {
       this.item.template_id = val;
     },
   },
-  created() {
+  async created() {
     this.item.template_id = this.templateId;
+    await this.getInventoryHosts();
   },
   methods: {
     getItemsUrl() {
       return `/api/project/${this.projectId}/tasks`;
     },
+    async getInventoryIdByTemplate() {
+      const template = (await axios({
+        method: 'get',
+        url: `/api/project/${this.projectId}/templates/${this.item.template_id}`,
+        responseType: 'json',
+      })).data;
+      return template.inventory_id;
+    },
+    async getInventoryHosts() {
+      const inventoryId = await this.getInventoryIdByTemplate();
+      this.hosts = (await axios({
+        method: 'get',
+        url: `/api/project/${this.projectId}/inventory/${inventoryId}/hosts`,
+        responseType: 'json',
+      })).data;
+    },
+
   },
 };
 </script>
